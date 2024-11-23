c2c1663daa4cb7850cd05333d70c93242710d9c2
kjh997788 <kjh997788@naver.com>
2024-11-23 22:28:45 +0900

    [Bronze I] Title: 팰린드롬수, Time: 92 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s = br.readLine();
+            if (s.equals("0")) break;
+            boolean p = true;
+            for (int i = 0; i < s.length()/2; i ++) {
+                if (s.charAt(i) != s.charAt(s.length()-1-i)) {
+                    p = false;
+                    break;
+                }
+            }
+            if (p) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+}
