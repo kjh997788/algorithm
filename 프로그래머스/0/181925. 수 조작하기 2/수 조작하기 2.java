99d32e67375ce31a10d7b1b50138b4fd0cf6d3c9
kjh997788 <kjh997788@naver.com>
2024-11-23 18:52:13 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.23 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < numLog.length - 1; i++) {
+            switch (numLog[i+1] - numLog[i]) {
+                case 1:
+                    answer.append("w");
+                    break;
+                case -1:
+                    answer.append("s");
+                    break;
+                case 10:
+                    answer.append("d");
+                    break;
+                case -10:
+                    answer.append("a");
+                    break;
+            }
+        }
+        return answer.toString();
+    }
+}
