b6bf98a7206e903d609dce148f96fff391a63260
kjh997788 <kjh997788@naver.com>
2024-11-27 00:16:05 +0900

    [Bronze V] Title: 카드 게임, Time: 100 ms, Memory: 14180 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(A+B+C+D+E));
+
+        bw.close();
+        br.close();
+    }
+}
