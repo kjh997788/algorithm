06426b4e66f16eb656938fb0ca2af53483fd0d71
kjh997788 <kjh997788@naver.com>
2024-12-02 13:44:34 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) answer.add(todo_list[i]);
+        }
+        return answer;
+    }
+}
