31ac61cb6d7580deac257ddf9aced55806625ef4
kjh997788 <kjh997788@naver.com>
2024-12-07 12:41:13 +0900

    [level 0] Title: 배열 만들기 6, Time: 36.44 ms, Memory: 146 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (stk.isEmpty()) stk.add(arr[i]);
+            else {
+                if(stk.get(stk.size() - 1) == arr[i]) stk.remove(stk.size() - 1);
+                else stk.add(arr[i]);
+            }
+        }
+        if (stk.isEmpty()) stk.add(-1);
+        return stk;
+    }
+}
