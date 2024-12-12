96943a392f329909ec799fc8ca18508cf17dda31
kjh997788 <kjh997788@naver.com>
2024-12-12 16:49:31 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.06 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            answer[i][j] = 1;
+            j++;
+        }
+        return answer;
+    }
+}
