c6a83f9e3c73aea87768aa3f14fd696f117be2f3
kjh997788 <kjh997788@naver.com>
2024-12-16 16:43:17 +0900

    [Silver II] Title: 최대 힙, Time: 312 ms, Memory: 27048 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (maxHeap.isEmpty()) bw.write("0\n");
+                else bw.write(maxHeap.poll() + "\n");
+            } else {
+                maxHeap.add(x);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
