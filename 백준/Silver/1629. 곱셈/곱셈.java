b56c03826b32fdd63f5d054083f9781ad4f1ae2c
kjh997788 <kjh997788@naver.com>
2024-12-29 17:42:38 +0900

    [Silver I] Title: 곱셈, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long result = 1;
+        A = A % C;
+
+        while (B > 0) {
+            if (B % 2 == 1) {
+                result = (result * A) % C;
+            }
+            A = (A * A) % C;
+            B /= 2;
+        }
+
+        bw.write((result % C) +"");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
